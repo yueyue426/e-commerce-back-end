f919512dc2fc400b80dc35e1b6eaf3fe2839e0d4
Chloe Jiang <yueyue426@berkeley.edu>
2024-07-28 16:49:17 -0700

    add tag_id property

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -19,6 +19,13 @@ ProductTag.init(
         model: 'product',
         key: 'id'
       },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      },
     }
   },
   {
