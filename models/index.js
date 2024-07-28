0468f39ca20562c8feec59804821f3c7f4a5e728
Chloe Jiang <yueyue426@berkeley.edu>
2024-07-28 16:49:40 -0700

    fix bugs

--- models/index.js
+++ models/index.js
@@ -16,10 +16,20 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, { 
+  through: {
+    model: ProductTag,
+    unique: false 
+  }
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, { 
+  through: {
+    model: ProductTag,
+    unique: false
+   }
+});
 
 module.exports = {
   Product,
